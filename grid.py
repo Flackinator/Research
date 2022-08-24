be5a60f951e0c7b7d8fe32cf114aa42c3dec6680
flackinator@DESKTOP-97M0E3B.localdomain <flackinator@DESKTOP-97M0E3B.localdomain>
2022-08-24 21:54:07 +1000

    y

--- grid.py
+++ grid.py
@@ -138,7 +138,7 @@ class Quad(object):
         ro = m.ron
         vo = m.von
         # TODO - test whether moon is further from earth than (solar) hill radius
-        if ((ii := np.argmax(ro[0, :] > 0.1 * AU)) > 0):
+        if ((ii := np.argmax(ro[0, :] > (['binary.1.an_km']/AU)*(['star.2.M_kg']/(3*['star.1.M_kg']))**(1/3))) > 0):
             return Outcome(Fate.MOONGONE, m.t[ii])
         # TODO - test whether moon and earth escaped jointly
         if ((ii := np.argmax(ro[2, :] > 2 * AU)) > 0):
